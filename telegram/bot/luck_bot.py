e3d19eaf2c1a3b0d2a015efe9c1fc7d2180c0035
h0han <hyhan@dgist.ac.kr>
2022-01-25 11:58:39 +0900

    Revised on Jan 25

--- telegram/bot/luck_bot.py
+++ telegram/bot/luck_bot.py
@@ -518,10 +518,10 @@ def presale(update, context):
 
 def foo(update, context):
     bot = telepot.Bot(TOKEN)
-    btn = BT(text = "🍀 Visit LUCK WHITELIST page 🍀", url = "", callback_data = "1")
+    btn = BT(text = "🍀 Send marketing proposal to cleaver 🍀", url = "", callback_data = "1")
     mu = MU(inline_keyboard = [[btn]])
     bot.sendMessage(chat_id = '@official_LUCK_community',
-                   text = "*✨ WHITELIST ANNOUNCEMENT ✨*\n\n*Please join the campaign quickly ：*\n👉 (link)\n\n🍀 Participate to be on the $LUCK Whitelist!\n🍀 Apply for the whitelist, create your referral link, and get $LUCK!\n\n*⏰ DEADLINE ⏰*\nWhitelist registration ends at: 09:00 AM UTC, February 10th.\n\nWINNERS WILL BE ANNOUNCED OUR OFFICIAL CHANNEL AFTER THE END OF THIS EVENT\n\n*🗣 The more friends you refer, greater the chances you'll win!*\n🔸 1st place: 1 BNB (full allocation)\n🔸 2nd place: 0.7 BNB (half allocation)\n🔸 3rd place:  0.3 BNB (one third allocation)\n\n",
+                   text = "If you have a marketing proposal to present to Project LUCK, DM LUCK_clever. *Clever* will kindly inform you of the marketing method using Chain Letter Protocol.",
                    reply_markup = mu,
                    parse_mode = "Markdown")
 
