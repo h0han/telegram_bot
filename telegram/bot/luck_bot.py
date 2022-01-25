41222c4d255584bfa181043663585403c76fe3ec
h0han <hyhan@dgist.ac.kr>
2022-01-25 15:47:47 +0900

    no

--- telegram/bot/luck_bot.py
+++ telegram/bot/luck_bot.py
@@ -521,10 +521,11 @@ def foo(update, context):
     bot = telepot.Bot(TOKEN)
     btn = BT(text = "🍀 Visit LUCK WHITELIST page 🍀", url = "", callback_data = "1")
     mu = MU(inline_keyboard = [[btn]])
-    bot.sendMessage(chat_id = '@official_LUCK_community',
-                   text = "If you have a marketing proposal to present to Project LUCK, DM LUCK_clever. *Clever* will kindly inform you of the marketing method using Chain Letter Protocol.\n\nIf you have a marketing proposal to present to Project LUCK, DM LUCK_clever. *Clever* will kindly inform you of the marketing method using Chain Letter Protocol.\nIf you have a marketing proposal to present to Project LUCK, DM LUCK_clever. *Clever* will kindly inform you of the marketing method using Chain Letter Protocol.\n",
-                   reply_markup = mu,
-                   parse_mode = "Markdown")
+    bot.sendVideo(chat_id = '@official_LUCK_community',
+        video="https://t.me/official_LUCK_community/282",
+        caption="*💖 WHY IS $LUCK TO SUCCESS? 💖*\n🎁 Chain Letter Protocol(CLP) is the most powerful marketing protocol covering crypto & real world \n🎁 The amazing reward system for ALL CONTRIBUTORS, sender, receiver, and staker \n🎁 REFERRAL REWARDS : 💵Sender💵 🔗 💌Lucky Chain Letter💌 🔗 💶Receiver💶 \n\n*💰 $LUCK Token 💰*\n💵 1. REWARD for CONTRIBUTORS : Pledge referral and mission rewards to senders and receivers, Various benefits \n💶 2. STAKING : Apply a high level of consistent APY \n💷 3. BUYBACK : $LUCK continues to increase in value through Buyback\n💴 4. TIER System : The more lucky chain letters you spread, the higher the tier you are assigned, which leads to higher rewards\n\n",
+        reply_markup = mu,
+        parse_mode = "Markdown")
 
 def main():
     # Create the Updater and pass it your bot's token.
@@ -539,7 +540,7 @@ def main():
     dp.add_handler(CommandHandler("airdrop", airdrop))
     dp.add_handler(CommandHandler("whitelist", whitelist))
     dp.add_handler(CommandHandler("presale", presale))
-    dp.add_handler(CommandHandler("a", foo))
+    dp.add_handler(CommandHandler("aaa", foo))
     # dp.add_handler(CommandHandler("keyword", keyword))
     # dp.add_handler(CommandHandler("marketing", sir))
     # dp.add_handler(CommandHandler("proposal", sir))
